af24f75df3aec8d74b48cb05c6fefab1873f672c
Kiwi Ganesh <kirisanth.ganeshamoorthy@partnerstack.com>
2020-08-17 17:59:31 -0400

    Revert "make it a module"
    This reverts commit 2da465b21bbe23691a99ef523cf9192f3f86d86e.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import "@actions/core";
-import "@actions/github";
+const core = require("@actions/core");
+const github = require("@actions/github");
 
 try {
   // `who-to-greet` input defined in action metadata file
