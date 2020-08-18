628c3a82a338ea67ceff230982d79ca044e5fe1f
Kiwi Ganesh <kirisanth.ganeshamoorthy@partnerstack.com>
2020-08-18 17:14:26 -0400

    Remove comments and some logs

--- index.js
+++ index.js
@@ -2,19 +2,6 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 const { Octokit } = require("@octokit/rest");
 
-// try {
-//   // `who-to-greet` input defined in action metadata file
-//   const nameToGreet = core.getInput("who-to-greet");
-//   console.log(`Hello ${nameToGreet}!`);
-//   const time = new Date().toTimeString();
-//   core.setOutput("time", time);
-//   // Get the JSON webhook payload for the event that triggered the workflow
-//   const payload = JSON.stringify(github.context.payload, undefined, 2);
-//   console.log(`The event payload: ${payload}`);
-// } catch (error) {
-//   core.setFailed(error.message);
-// }
-
 async function main() {
   const token = process.env.GITHUB_TOKEN;
   const label = core.getInput("label");
@@ -45,7 +32,6 @@ async function getPRs(octokit, url) {
 }
 
 function filterPRByLabel(prs, input_label) {
-  console.log(prs, input_label);
   return prs.filter(function (pr) {
     return pr.labels.some((label) => label.name == input_label);
   });
